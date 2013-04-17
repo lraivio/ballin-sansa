52b6b3028ee361e6074ed51d6f47a14ae0f42af2
Oskari Hiltunen <git@loethr.net>
2013-04-18 01:33:09 +0300

    Save the drawn stuff and send it to joining players.
    Todo: Clear on round end.

--- socket.js
+++ socket.js
@@ -1,7 +1,11 @@
 module.exports = function (io) {
     var count = 0;
+    var drawing = [];
     io.sockets.on('connection', function (socket) {
         count += 1;
+        if (drawing) {
+            socket.emit('draw', drawing);
+        }
 
         socket.on('disconnect', function () {
             count -= 1;
@@ -9,6 +13,7 @@ module.exports = function (io) {
 
         if (count == 1) {
             socket.on('draw', function (data) {
+                drawing.push.apply(drawing, data);
                 socket.broadcast.emit('draw', data);
             });
         }
