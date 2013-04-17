9d74f5a3c9e3e3035853e892dd68c86746b30619
Oskari Hiltunen <git@loethr.net>
2013-04-17 23:25:41 +0300

    Add asset management.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 var app = express();
 
+// Serve the index view.
 app.get('/', function (req, res) {
     res.set('Content-Type', 'text/html');
     fs.readFile('./index.html', function (err, data) {
@@ -12,6 +13,11 @@ app.get('/', function (req, res) {
         res.send(data);
     });
 });
+
+// Serve assets.
+var assetGroups = require('./assets');
+var assetManager = require('connect-assetmanager');
+app.use(assetManager(assetGroups));
 
 app.listen(8080);
 console.log('Listening on port 8080.');
---
+++ assets.js
@@ -0,0 +1,35 @@
+var less = require('less');
+
+module.exports = {
+    'js': {
+        'route': /\/static\/site\.js/,
+        'path': './static/javascripts/',
+        'dataType': 'javascript',
+        'files': [
+            'vendor/jquery-1.9.1.js',
+            '*',
+        ],
+    },
+    'css': {
+        'route': /\/static\/site\.css/,
+        'path': './static/stylesheets/',
+        'dataType': 'css',
+        'files': [
+            '*',
+        ],
+        'preManipulate': {
+            '^': [
+                function (content, path, index, last, callback) {
+                    // Compile .less files.
+                    if (path.slice(-5) == '.less')
+                        less.render(content, function (err, content) {
+                            if (err) throw err;
+                            callback(content);
+                        });
+                    else
+                        callback(content);
+                },
+            ],
+        },
+    },
+};
